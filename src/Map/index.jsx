198484380bfddab252bab62c840ba883951ae036
Denisa Plačková <dejna.plackova@gmail.com>
2022-06-04 15:08:45 +0200

    filtrování

--- src/Map/index.jsx
+++ src/Map/index.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import "./style.css";
-import "mapbox-gl/dist/mapbox-gl.css";
-import { useState, useEffect } from "react";
-import ReactMapGL, { Marker, Popup, NavigationControl } from "react-map-gl";
-import spendlikUrl from "./img/spendlik.svg";
-
-/*const mista = [
-	{
-		id: 1,
-		ikonaUrl: spendlikUrl,
-		latitude: 50.05872180714727,
-		longitude: 14.320820395331165,
-	},
-	{
-		id: 2,
-		ikonaUrl: spendlikUrl,
-		latitude: 50.05758297186479,
-		longitude: 14.324045362741034,
-	},
-]*/
+import React from 'react';
+import './style.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { useState, useEffect } from 'react';
+import ReactMapGL, { Marker, Popup, NavigationControl } from 'react-map-gl';
+import spendlikUrl from './img/spendlik.svg';
+import cx from 'classnames';
 
 export const Map = () => {
   const [viewport, setViewport] = useState({
-    latitude: 50.05872180714727,
-    longitude: 14.320820395331165,
-    zoom: 12,
+    latitude: 50.03372651009443,
+    longitude: 14.595659182553074,
+    zoom: 8,
   });
   const [popupOtevren, setPopupOtevren] = useState(null);
   const [mista, setMista] = useState([]);
+  const [filter, setFilter] = useState({
+    veterina: true,
+    nonstop: true,
+    zachranka: true,
+    cvicak: true,
+    hotel: true,
+  });
+  const emptyFilter = {
+    veterina: false,
+    nonstop: false,
+    zachranka: false,
+    cvicak: false,
+    hotel: false,
+  };
   const dataUrl =
-    "https://sheets.googleapis.com/v4/spreadsheets/1IJoc0OI8JyPvLLJFr_9u45UiiK5EpFMoiJWB12V_2QM?key=AIzaSyDIwamOn-0qkbyAJmujKElLuL5wk_udHNE&includeGridData=true";
+    'https://sheets.googleapis.com/v4/spreadsheets/1IJoc0OI8JyPvLLJFr_9u45UiiK5EpFMoiJWB12V_2QM?key=AIzaSyDIwamOn-0qkbyAJmujKElLuL5wk_udHNE&includeGridData=true';
 
   useEffect(() => {
     fetch(`${dataUrl}`)
@@ -41,22 +41,79 @@ export const Map = () => {
               name: row.values[0].effectiveValue.stringValue,
               type: row.values[1].effectiveValue.stringValue,
               address: row.values[2].effectiveValue.stringValue,
-              latitude: parseFloat(row.values[3].effectiveValue.stringValue.split(', ')[0]),
-              longitude: parseFloat(row.values[3].effectiveValue.stringValue.split(', ')[1]),
+              latitude: parseFloat(
+                row.values[3].effectiveValue.stringValue.split(', ')[0],
+              ),
+              longitude: parseFloat(
+                row.values[3].effectiveValue.stringValue.split(', ')[1],
+              ),
               url: row.values[4].effectiveValue.stringValue,
               description: row.values[5].effectiveValue.stringValue,
-
             };
-          })
+          }),
         );
       });
   }, [setMista]);
 
-  console.log(mista)
+  console.log(mista);
 
   return (
     <section className="main--section__map">
-      <h1>Psí mapa</h1>
+      <div>
+        <button
+          onClick={() =>
+            setFilter({
+              veterina: true,
+              nonstop: true,
+              zachranka: true,
+              cvicak: true,
+              hotel: true,
+            })
+          }
+          className={
+            filter.zachranka &&
+            filter.hotel &&
+            filter.nonstop &&
+            filter.cvicak &&
+            filter.veterina
+              ? 'isActive'
+              : ''
+          }
+        >
+          Vše
+        </button>
+        <button
+          onClick={() => setFilter({ ...emptyFilter, veterina: true })}
+          className={filter.veterina ? 'isActive' : ''}
+        >
+          veterina
+        </button>
+        <button
+          onClick={() => setFilter({ ...emptyFilter, nonstop: true })}
+          className={filter.nonstop ? 'isActive' : ''}
+        >
+          nonstop veterina
+        </button>
+        <button
+          onClick={() => setFilter({ ...emptyFilter, zachranka: true })}
+          className={filter.zachranka ? 'isActive' : ''}
+        >
+          {' '}
+          zvířecí záchranka
+        </button>
+        <button
+          onClick={() => setFilter({ ...emptyFilter, cvicak: true })}
+          className={filter.cvicak ? 'isActive' : ''}
+        >
+          cvičák
+        </button>
+        <button
+          onClick={() => setFilter({ ...emptyFilter, hotel: true })}
+          className={filter.hotel ? 'isActive' : ''}
+        >
+          psí hotel
+        </button>
+      </div>
       <ReactMapGL
         {...viewport}
         width="100%"
@@ -65,9 +122,9 @@ export const Map = () => {
         mapStyle={{
           version: 8,
           sources: {
-            "raster-tiles": {
-              type: "raster",
-              tiles: ["https://mapserver.mapy.cz/base-m/{z}-{x}-{y}"],
+            'raster-tiles': {
+              type: 'raster',
+              tiles: ['https://mapserver.mapy.cz/base-m/{z}-{x}-{y}'],
               tileSize: 256,
               attribution:
                 'Mapové podklady od <a target="_top" rel="noopener" href="https://mapy.cz/">Seznam.cz</a> a <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>.',
@@ -75,9 +132,9 @@ export const Map = () => {
           },
           layers: [
             {
-              id: "simple-tiles",
-              type: "raster",
-              source: "raster-tiles",
+              id: 'simple-tiles',
+              type: 'raster',
+              source: 'raster-tiles',
               minzoom: 0,
               maxzoom: 18,
             },
@@ -85,24 +142,65 @@ export const Map = () => {
         }}
       >
         <div className="control">
-	        <NavigationControl />
+          <NavigationControl />
         </div>
-        {mista.map((misto, index) => (<Marker
-            key={index}
-            latitude={misto.latitude}
-            longitude={misto.longitude}
-            offsetLeft={-15}
-            offsetTop={-15}
-            onClick={() => setPopupOtevren(misto)}
+        {mista
+          .filter((misto) => {
+            const cvicak = misto.type.includes('cvicak');
+            const hotel = misto.type.includes('hotel');
+            const nonstop = misto.type.includes('nonstop');
+            const zachranka = misto.type.includes('zachranka');
+            const veterina = misto.type.includes('veterina');
 
-          >
-            <img src={spendlikUrl} width={30} height={30} alt="" />
-          </Marker>)
-        )}
+            let pass = false;
+
+            if (filter.cvicak) {
+              pass = pass || cvicak;
+            }
+            if (filter.hotel) {
+              pass = pass || hotel;
+            }
+            if (filter.nonstop) {
+              pass = pass || nonstop;
+            }
+            if (filter.zachranka) {
+              pass = pass || zachranka;
+            }
+            if (filter.veterina) {
+              pass = pass || veterina;
+            }
+            return pass;
+          })
+          .map((misto, index) => {
+            const cvicak = misto.type.includes('cvicak');
+            const hotel = misto.type.includes('hotel');
+            const nonstop = misto.type.includes('nonstop');
+            const zachranka = misto.type.includes('zachranka');
+            const veterina = misto.type.includes('veterina');
+            return (
+              <Marker
+                key={index}
+                latitude={misto.latitude}
+                longitude={misto.longitude}
+                offsetLeft={-15}
+                offsetTop={-15}
+                onClick={() => setPopupOtevren(misto)}
+                className={cx({
+                  'type-cvicak': cvicak,
+                  'type-hotel': hotel,
+                  'type-nonstop': nonstop,
+                  'type-zachranka': zachranka,
+                  'type-veterina': veterina,
+                })}
+              >
+                <img src={spendlikUrl} width={30} height={30} alt="" />
+              </Marker>
+            );
+          })}
         {popupOtevren && (
           <Popup
             onClose={() => setPopupOtevren(null)}
-            latitude= {popupOtevren.latitude}
+            latitude={popupOtevren.latitude}
             longitude={popupOtevren.longitude}
             offsetTop={-15}
           >
