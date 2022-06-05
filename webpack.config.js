c68f1c1a1c39371de601f3ddef57c185c6be165d
Lada Šilhavá <lada.silhava@email.cz>
2022-06-05 13:03:46 +0200

    webpack mode production

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.jsx',
   devtool: 'eval-source-map',
   output: {
