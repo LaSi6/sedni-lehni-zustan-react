e2f349b04be3dd34ea87c80e6d985414cc4d10e6
Lada Šilhavá <lada.silhava@email.cz>
2022-06-05 12:22:12 +0200

    dostylování kontaktů

--- src/Contacts/index.jsx
+++ src/Contacts/index.jsx
@@ -3,8 +3,10 @@ import './style.css';
 import { useNavigate } from 'react-router-dom';
 
 const Contacts = () => (
-  <>
+  <div className="main--contacts">
     <section className="main--section__contacts">
+    <div className='img--contacts img--contacts__denisa'></div>
+    <div className="main--text__contacts">
       <h1 className="text--headline__contacts">Denisa Plačková</h1>
       <div className="text--contacts">
         <h2 className="text--subheadline2__contacts">IČO:</h2>
@@ -19,12 +21,15 @@ const Contacts = () => (
         <div className="contact">+420 777 303 211</div>
       </div>
       <div className="text--contacts">
-        <h2 className="text--subheadline2__contacts">místo působnosti:</h2>
+        <h2 className="text--subheadline2__contacts">působnost:</h2>
         <div className="contact">Praha a přilehlé okolí</div>
       </div>
+    </div>
     </section>
 
     <section className="main--section__contacts">
+    <div className='img--contacts img--contacts__tomas'></div>
+    <div className="main--text__contacts">
       <h1 className="text--headline__contacts">Tomáš Dobiáš</h1>
       <div className="text--contacts">
         <h2 className="text--subheadline2__contacts">IČO:</h2>
@@ -39,11 +44,12 @@ const Contacts = () => (
         <div className="contact">+420 736 177 353</div>
       </div>
       <div className="text--contacts">
-        <h2 className="text--subheadline2__contacts">místo působnosti:</h2>
+        <h2 className="text--subheadline2__contacts">působnost:</h2>
         <div className="contact">Kouřim a přilehlé okolí</div>
       </div>
+    </div>
     </section>
-  </>
+  </div>
 );
 
 export default Contacts;
